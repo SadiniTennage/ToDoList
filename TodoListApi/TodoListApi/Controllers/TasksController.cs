1d16963e70fd68aff31c77caf59662777a11742f
SadiniTennage <sadiniisurika@gmail.com>
2024-04-30 22:42:43 +0530

    Add files via upload

---
+++ TodoListApi/TodoListApi/Controllers/TasksController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoListApi.Models;
+using Task = TodoListApi.Models.Task;
+
+namespace TodoListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static List<Task> tasks = new List<Task>();
+        private static int nextId = 1;
+
+        // GET: api/tasks
+        [HttpGet]
+        public ActionResult<IEnumerable<Task>> GetTasks()
+        {
+            return Ok(tasks);
+        }
+
+        // POST: api/tasks
+        [HttpPost]
+        public ActionResult<Task> AddTask(Task task)
+        {
+            task.Id = nextId++;
+            tasks.Add(task);
+            return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+        }
+
+        // GET: api/tasks/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Task> GetTask(int id)
+        {
+            var task = tasks.Find(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return Ok(task);
+        }
+
+        // DELETE: api/tasks/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteTask(int id)
+        {
+            var task = tasks.Find(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            tasks.Remove(task);
+            return NoContent();
+        }
+    }
+}
---
+++ TodoListApi/TodoListApi/Models/Task.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoListApi.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
